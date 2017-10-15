9baa7b42d582b925e6c19524e0bafd6b8f25ac7b
woogii <jhsw0201@naver.com>
2017-10-15 15:24:22 +0200

    chore : Delete a file that was ignored by .gitignore

--- GiniCats/GiniCats/Constant.swift
+++ GiniCats/GiniCats/Constant.swift
@@ -68,7 +68,7 @@ struct Constants {
       static let ActionKey           = "action"
       static let ActionValue         = "remove"
       static let APIKey              = "api_key"
-      static let APIValue            = Secret.APIValue
+      static let APIValue            = ""
       static let ImageIdKey          = "image_id"
       static let SizeKey             = "size"
       static let SizeValue           = "small"
