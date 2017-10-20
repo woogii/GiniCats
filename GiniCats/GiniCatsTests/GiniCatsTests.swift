6a2e21e019700ed17643158565b6657f3c41e04f
woogii <jhsw0201@naver.com>
2017-10-20 13:18:09 +0200

    feat : Implement test code for RestClientConvenience methods

--- GiniCats/GiniCatsTests/GiniCatsTests.swift
+++ GiniCats/GiniCatsTests/GiniCatsTests.swift
@@ -7,29 +7,96 @@
 //
 
 import XCTest
+@testable import GiniCats
 
 class GiniCatsTests: XCTestCase {
 
+  let apiKey = ""
+  var sessionUnderTest: URLSession!
+  var restClient: RestClient!
+  var favoringImageIsFavorite: Bool!
+  var favoringImageId: String!
+
   override func setUp() {
     super.setUp()
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+
+    sessionUnderTest = URLSession(configuration: URLSessionConfiguration.default)
+    restClient = RestClient()
+    favoringImageIsFavorite = true
+    favoringImageId = "ba1"
   }
 
   override func tearDown() {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
     super.tearDown()
+    sessionUnderTest = nil
+    restClient = nil
+    favoringImageIsFavorite = nil
+    favoringImageId = nil
   }
 
-  func testExample() {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-  }
+  func testValidCallToCatListGetHTTPStatusCode200() {
 
-  func testPerformanceExample() {
-    // This is an example of a performance test case.
-    self.measure {
-      // Put the code you want to measure the time of here.
+    let promise = expectation(description: "Receive cat list xml data")
+
+    restClient.requestCatImageInfoList { (_, error) in
+      if let error = error {
+        XCTFail("\(error.localizedDescription)")
+      } else {
+        promise.fulfill()
+      }
     }
+    waitForExpectations(timeout: 5, handler: nil)
+  }
+
+  func testValidCallToFavoriteCatListGetHTTPStatusCode200() {
+
+    let promise = expectation(description: "Receive favorite cat list xml data")
+
+    restClient.requestFavoriteCatImageInfoList { (_, error) in
+      if let error = error {
+        XCTFail("\(error.localizedDescription)")
+      } else {
+        promise.fulfill()
+      }
+    }
+    waitForExpectations(timeout: 5, handler: nil)
+  }
+
+  func testValidCallToMarkFavoriteCatGetResponse() {
+
+    let promise = expectation(description: "Mark favorite cat")
+
+    print(favoringImageIsFavorite)
+    restClient.requestFavoringImageBasedOn(isFavorite: favoringImageIsFavorite, imageId: favoringImageId) { response in
+      if let response = response {
+        XCTFail("\(response)")
+      } else {
+        promise.fulfill()
+        self.favoringImageIsFavorite = !self.favoringImageIsFavorite
+      }
+    }
+    waitForExpectations(timeout: 5, handler: nil)
+  }
+
+  func testValidCallToUndoMarkFavoriteCatGetHTTPStatusCode200() {
+    let baseUrl = "http://thecatapi.com/api/images/favorite?api_key=\(apiKey)&sub_id=siwook&image_id=act"
+    let undoMarkFavoriteQuery = "&action=remove"
+
+    let url = URL(string: baseUrl + undoMarkFavoriteQuery)
+    let promise = expectation(description: "Status code: 200")
+
+    let dataTask = sessionUnderTest.dataTask(with: url!) { _, response, error in
+      if let error = error {
+        XCTFail("\(error.localizedDescription)")
+      } else if let httpResponse = response as? HTTPURLResponse {
+        if 200..<300 ~= httpResponse.statusCode {
+          promise.fulfill()
+        }
+      }
+    }
+
+    dataTask.resume()
+    waitForExpectations(timeout: 5, handler: nil)
   }
 
 }
